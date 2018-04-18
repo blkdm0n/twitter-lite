126c1534971a4d00fa37c80ce6d7f02ff1d4c692
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-04-18 16:22:11 -0700

    basic functionality is enabled

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
-
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + `/public`));
-
 var tweets = [
-  
-]
+  {text: "What up buttercup?", time: new Date().getTime() - 12300},
+  {text: "Howdy doo cockatoo?", time: new Date().getTime() - 1000},
+  {text: "Good day sir!", time: new Date().getTime()},
+];
 
-app.get
+app.use(express.static(__dirname + "/public"));
+
+app.get("/ajax", (req, res) => {
+  res.type('json');
+  res.end(JSON.stringify({tweets:tweets}));
+});
+
+app.post("/ajax", (req, res) => {
+  var newTweet = {text:req.body.tweet, time: new Date().getTime()};
+  tweets.push(newTweet);
+  res.type('json');
+  res.end(JSON.stringify(newTweet));
+});
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Your Express server is running on port ${port}.  You better catch it!`);
+});
--- public/tweet.js
+++ public/tweet.js
@@ -1,6 +1,6 @@
 $.ajax({
   type: "GET",
-  url: "/retrieve",
+  url: "/ajax",
   success: function(data) {
     for (let i = 0; i < data.tweets.length; i++) {
       appendNewTweet(data.tweets[i]);
@@ -9,17 +9,23 @@ $.ajax({
 });
 
 function appendNewTweet(tweet) {
-  var newTweet = `
-  <div class="tweet-container"> 
-    <div class="tweet-time"> ${new Date(tweet.time).toLocaleString} </div> 
-    <div class="tweet-body"> ${tweet.text} </div> 
-  </div>`;
+  var newTweet =  "<div class='tweet-container'>" +
+    "<div class='tweet-time'>" + new Date(tweet.time).toLocaleString() + "</div>" +
+    "<div class='tweet-body'>" + tweet.text + "</div>" +
+    "</div>";
   //prepend adds the latest tweet to the top (as opposed to append)
   $('#tweets-target').prepend(newTweet);
 }
 
 $('#tweet').click(function() {
   $.ajax({
-    
+    type: "POST",
+    url: "/ajax",
+    contentType: "application/json",
+    data: JSON.stringify({tweet: $("#new-tweet").val()}),
+    success: function(data) {
+      appendNewTweet(data);
+      $("#new-tweet").val("");
+    }
   })
-})
+});
